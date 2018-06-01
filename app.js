c4dcb91ac41ba09a8f1f229b168d27d5c5705112
Gaurav Kumar <gaurav.kr.9637@gmail.com>
2018-06-01 18:10:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+mongoose.connect('mongodb://localhost/mern-crud')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+var book = require('./routes/book');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/book', book);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
